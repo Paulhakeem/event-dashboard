7c309bf91a6c71fd609549760bcc59a389ec774d
Paul <paulnyamawi18@gmail.com>
2025-09-18 21:41:26 +0300

    adding shadcn

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <!-- background image -->
+    <div class="relative h-96 w-full">
+      <img
+        src="/images/dj.jpg"
+        alt="Event Background"
+        class="absolute inset-0 h-full w-full object-cover"
+      />
+      <div class="absolute inset-0 flex items-center justify-center md:justify-start lg:justify-start">
+        <div class="md:pl-8 lg:pl-8">
+          <h1 class="text-white text-xl pb-3 md:text-5xl lg:text-6xl font-semibold">Events</h1>
+          <button
+            class="ml-auto rounded-md bg-blue-600 px-3 py-2 items-center text-white hover:bg-blue-700"
+          >
+            Create Event
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/index.css"],
+  modules: ["shadcn-nuxt"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./app/components/ui",
+  },
+  components: {
+    dirs: [
+      "~/components", // auto-import your own components
+      { path: "~/app/components/ui", global: false , extensions: [".vue"]}, // disable auto-import for shadcn-vue
+    ],
+  },
+});
