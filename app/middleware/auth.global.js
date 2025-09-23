cdc34738de869ba96790bc92d636c9ab85063343
Paul <paulnyamawi18@gmail.com>
2025-09-23 23:17:28 +0300

    fixing some errors

--- app/middleware/auth.global.js
+++ app/middleware/auth.global.js
@@ -1,22 +1,16 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const role = process.client ? localStorage.getItem("role") : null;
-  const token = process.client ? localStorage.getItem("token") : null;  
-    const publicPages = ["/login", "/signup"];
-    const authRequired = !publicPages.includes(to.path);
-    if (authRequired && !token) {
-        return navigateTo('/login');
+  const token = process.client ? localStorage.getItem("token") : null;
+
+// Protect admin routes
+  if (to.path.startsWith("/admin") && role !== "admin") {
+    return navigateTo("/login");
+  }
+  if ((to.path === "/login" || to.path === "/signup") && token) {
+    if (role === "admin") {
+      return navigateTo("/admin/dashboard");
+    } else {
+      return navigateTo("/user/dashboard");
     }
-    if (to.path.startsWith('/admin') && role !== 'admin') {
-        return navigateTo('/login');
-    }
-    if (to.path.startsWith('/user') && role !== 'user') {
-        return navigateTo('/login');
-    }
-    if ((to.path === '/login' || to.path === '/signup') && token) {
-        if (role === 'admin') {
-            return navigateTo('/admin/dashboard');
-        } else {
-            return navigateTo('/user/dashboard');
-        }
-    }
+  }
 });
