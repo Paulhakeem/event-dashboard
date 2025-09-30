bc9c0b46e2fee1a2c1694269dced39e37f6b4e2f
Paul <paulnyamawi18@gmail.com>
2025-09-30 22:49:13 +0300

    done with users page

---
+++ app/composables/totalUsers.js
@@ -0,0 +1,46 @@
+export const totalUsers = () => {
+  const users = ref([
+    {
+      img: "https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+      name: "Christina Bersh",
+      email: "christina@site.com",
+      role: "user",
+      events: 10,
+      joined: "2/7/2026",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+      name: "David Harrison",
+      email: "davida@site.com",
+      role: "user",
+      events: 10,
+      joined: "2/7/2026",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+      name: "Christina Bersh",
+      email: "christina@site.com",
+      role: "Admin",
+      events: 10,
+      joined: "2/7/2026",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+      name: "Christina Bersh",
+      email: "christina@site.com",
+      role: "user",
+      events: 10,
+      joined: "2/7/2026",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+      name: "Christina Bersh",
+      email: "christina@site.com",
+      role: "user",
+      events: 10,
+      joined: "2/7/2026",
+    },
+  ]);
+
+  return { users };
+};
