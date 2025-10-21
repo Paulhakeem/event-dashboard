41cc7e5f868ca8d0789a96e873e2a38ff5d09592
Paul <paulnyamawi18@gmail.com>
2025-10-21 22:39:32 +0300

    adding payment information

--- app/pages/events/[id].vue
+++ app/pages/events/[id].vue
@@ -6,7 +6,7 @@
         <h2
           class="text-[#9c4e8b] font-semibold text-2xl md:text-4xl md:leading-tight"
         >
-            Event Details
+          Event Details
         </h2>
       </div>
       <!-- End Title -->
@@ -92,6 +92,55 @@
               </ul>
             </dd>
           </dl>
+
+          <!-- payment infor -->
+          <div class="mt-4 pt-4 border-t border-gray-300">
+            <h3 class="text-lg font-medium text-gray-800 dark:text-neutral-200">
+              Mpesa Payment Information
+            </h3>
+          </div>
+
+          <!-- user information eg name and email -->
+          <dl class="flex sm:flex-row gap-1">
+            <dt class="min-w-40">
+              <span class="block text-sm text-gray-500 dark:text-neutral-500"
+                >Name:</span
+              >
+            </dt>
+            <dd>
+              <ul>
+                <li
+                  class="me-1 after:content-[','] inline-flex items-center text-sm text-gray-800 dark:text-neutral-200"
+                >
+                  John Doe
+                </li>
+              </ul>
+            </dd>
+          </dl>
+          <dl class="flex sm:flex-row gap-1">
+            <dt class="min-w-40">
+              <span class="block text-sm text-gray-500 dark:text-neutral-500"
+                >Email:</span
+              >
+            </dt>
+            <dd>
+              <ul>
+                <li
+                  class="me-1 after:content-[','] inline-flex items-center text-sm text-gray-800 dark:text-neutral-200"
+                ></li>
+              </ul>
+            </dd>
+          </dl>
+          <!-- number input for payment -->
+          <label
+            class="block text-sm font-medium text-gray-700 dark:text-neutral-300"
+            >Phone Number:</label
+          >
+          <input
+            type="text"
+            placeholder="1234 5678 9012 3456"
+            class="mt-1 block w-72 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-[#9c4e8b] focus:border-[#9c4e8b] sm:text-sm"
+          />
           <!-- button for price tag -->
           <div>
             <button
@@ -99,6 +148,23 @@
             >
               Buy Ticket - ksh{{ event?.price }}
             </button>
+          </div>
+          <!-- choose different method of payment -->
+          <div class="mt-6">
+            <h4
+              class="text-md font-medium text-gray-800 dark:text-neutral-200 mb-2"
+            >
+              Choose Payment Method:
+            </h4>
+            <div class="flex gap-4 items-center">
+              <div>
+               <Icon name="flowbite:visa-solid" class="text-5xl"/>
+              </div>
+              <!-- paypal -->
+              <div>
+               <Icon name="fontisto:paypal" class="text-3xl"/>
+                </div>
+            </div>
           </div>
         </div>
       </div>
