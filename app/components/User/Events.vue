8d70df58d48f21af9b5b6561e1b41ec752a8785b
Paulhakeem <paulnyamawi18@gmail.com>
2026-01-24 00:27:00 +0300

    done

--- app/components/User/Events.vue
+++ app/components/User/Events.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Events</div>
+  <div class="space-y-4">
+    <!-- Header -->
+    <div>
+      <h2 class="text-2xl font-bold text-gray-800 flex items-center gap-2">
+        <Icon name="mdi:ticket" class="text-[#9d4e8a]" />All Events
+      </h2>
+      <p class="text-sm text-gray-500">
+       Browse and discover events from all organizations
+      </p>
+    </div>
+  </div>
+
+  <!-- Events List -->
 </template>
 
 <script setup></script>
