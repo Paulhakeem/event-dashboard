9c8e7d63853e922def8ec8bc57bd396c62e14327
thecodingmontana <dev.humblechris254@gmail.com>
2026-05-17 16:32:39 +0300

    ci(app): updated the github action

---
+++ ecosystem.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: "velora-events",
+      port: 3005,
+      exec_mode: "cluster",
+      instances: 1,
+      script: "./.output/server/index.mjs",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3005,
+        UV_THREADPOOL_SIZE: 128,
+        NODE_OPTIONS: "--max-old-space-size=384",
+      },
+      env_file: ".env",
+      watch: false,
+      max_memory_restart: "512M",
+      restart_delay: 3000,
+      max_restarts: 10,
+      min_uptime: "10s",
+      out_file: "/var/log/pm2/velora-events-out.log",
+      error_file: "/var/log/pm2/velora-events-error.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      combine_logs: true,
+      merge_logs: true,
+      kill_timeout: 10_000,
+      shutdown_with_message: true,
+    },
+  ],
+};
